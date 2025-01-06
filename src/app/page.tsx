38af2080b63d7eee5c792f8d16727dcd0f083cf1
TADEJ_NITIC <nitic.tadej@gmail.com>
2025-01-06 17:38:10 +0100

    HomePage fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,21 @@
 export default function Home() {
   return (
     <div className="container mx-auto">
-      <h1>Welcome home!</h1>
+      <div>
+        <h1>Welcome home!</h1>
+        <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis a sapien nunc. Aliquam sit amet est nec mauris lacinia lacinia. Morbi semper, orci ac mattis commodo, felis ipsum ullamcorper dolor, vitae venenatis nisi diam at mauris. Nunc fringilla nisl tellus, blandit tincidunt lectus suscipit eu. Morbi finibus tortor sit amet tellus rutrum, vitae commodo orci eleifend. Cras risus turpis, vestibulum id est vel, auctor aliquet quam. Aliquam posuere orci eros. Donec eros sapien, condimentum ut gravida sed, dapibus id lorem. In semper tincidunt velit, eu congue felis fringilla in. Donec eget libero eu magna venenatis mattis convallis ac tellus. Fusce eget libero nec velit aliquet rhoncus. Morbi sagittis tristique purus. Morbi suscipit ipsum felis, eu commodo purus finibus eu. Ut aliquet aliquam elit, et finibus nulla convallis id. Morbi laoreet turpis purus, id dictum nulla blandit eget.
+
+Aenean rhoncus pretium odio eu fringilla. Integer pretium hendrerit lorem, vel interdum tellus pulvinar nec. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse tristique mattis augue, vel lacinia ipsum semper id. Mauris auctor tortor vitae mattis aliquet. Sed imperdiet diam urna, ut blandit nulla maximus eget. In vitae eleifend quam, ac convallis tellus. Donec auctor posuere dui, eget pellentesque dui viverra in. Ut sed diam vel nibh tincidunt tempus. Nullam elementum a tellus eget vehicula.
+
+Nunc rutrum tellus quis leo ultrices fermentum. Proin pellentesque leo et tincidunt tincidunt. Ut nunc dui, vestibulum ac feugiat ac, feugiat porttitor metus. Etiam eu arcu in risus semper porttitor. Sed tincidunt tempus quam non luctus. Aenean consequat scelerisque viverra. Mauris placerat, ex vitae fringilla semper, nibh libero feugiat enim, laoreet varius leo nibh ut sapien. In finibus nisi accumsan vehicula vulputate. Aenean nisi leo, scelerisque in tortor id, maximus tristique elit. Praesent ipsum nisl, sodales vitae gravida vitae, pulvinar id nibh. Aliquam eget arcu ac felis varius aliquet. Nam nisl eros, auctor vitae sagittis quis, condimentum sit amet enim. Quisque tellus dolor, congue vel ante quis, ullamcorper imperdiet nisi. Aenean id diam dapibus, accumsan ex facilisis, convallis neque.
+
+Morbi vulputate in enim in laoreet. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam rhoncus, diam sed cursus dapibus, risus nulla sollicitudin ipsum, ut porta lacus metus non erat. Nunc blandit, augue sit amet porttitor interdum, diam est aliquam purus, sit amet lacinia ante mi at felis. Donec at urna commodo mi iaculis porta a nec ligula. Mauris vehicula eros eu purus rhoncus, id mattis elit malesuada. Sed bibendum ex tellus, ut fringilla libero rhoncus nec. Fusce tincidunt enim viverra nunc tempor maximus. Vivamus eu rutrum ante. Nunc ut elementum eros. Fusce eget tincidunt augue. Cras tristique, elit nec facilisis accumsan, dolor turpis fermentum elit, non venenatis neque ante sit amet erat.
+
+Praesent dui sapien, efficitur sit amet ex at, interdum ultrices diam. Donec nec nunc a nulla ultrices consequat. Sed ornare mi lorem, finibus bibendum tellus condimentum at. Ut interdum massa eu leo viverra luctus. Nulla pulvinar urna id turpis facilisis convallis at non nulla. Donec rhoncus malesuada tempus. Etiam accumsan faucibus consectetur. In hac habitasse platea dictumst. Duis condimentum faucibus urna, hendrerit maximus urna lobortis eu. Curabitur quis diam massa. Donec ultricies lectus sit amet nisi pretium lobortis. Nunc at vulputate lectus. Praesent a rutrum metus. Cras accumsan, eros et elementum mattis, nisi turpis cursus eros, quis malesuada nisl eros a neque. Fusce ullamcorper maximus consequat.
+        </p>
+      </div>
+      
     </div>
   );
 }
